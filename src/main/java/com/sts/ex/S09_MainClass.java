e483ae22e07e16a90b7b2c194e3c9dff0f61297d
ahngee.song <ahngee.song@gmail.com>
2022-04-18 22:31:54 +0900

    example of lecture 9

---
+++ src/main/java/com/sts/ex/S09_MainClass.java
@@ -0,0 +1,26 @@
+package com.sts.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class S09_MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:s09_applicationCTX.xml");
+		
+		System.out.println("S09_MainClass.java");
+		System.out.println("================================");
+		
+		S09_Student student = ctx.getBean("student", S09_Student.class);
+		student.getStudentInfo();
+		
+		S09_Worker worker = ctx.getBean("worker", S09_Worker.class);
+		worker.getWorkerInfo();
+		System.out.println("================================");
+		
+		ctx.close();
+	}
+
+}
